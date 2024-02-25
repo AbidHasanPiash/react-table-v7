316a984e53f9cd6ada542aab80da23f7fe1615ca
AႦιԃ Hαʂαɳ <meghpiash@gmail.com>
2024-02-25 16:17:18 +0600

    [Add] column component

---
+++ components/table/columns.js
@@ -0,0 +1,7 @@
+export const COLUMN = [
+    {Header: 'ID', Accessor: 'id'},
+    {Header: 'First Name', Accessor: 'first_name'},
+    {Header: 'Last Name', Accessor: 'last_name'},
+    {Header: 'Email', Accessor: 'email'},
+    {Header: 'Gender', Accessor: 'gender'},
+]
