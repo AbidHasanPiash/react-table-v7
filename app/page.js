e2addf730f671dd99402488319b30c3845b4ba78
AႦιԃ Hαʂαɳ <meghpiash@gmail.com>
2024-02-26 11:16:41 +0600

    [ADD] select rows

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import BasicTable from "@/components/table/BasicTable";
 import FilteredTable from "@/components/table/FilteredTable";
 import PaginationTable from "@/components/table/PaginationTable";
+import RowSelectionTable from "@/components/table/RowSelectionTable";
 import SortedTable from "@/components/table/SortedTable";
 
 
 export default function Home() {
   return (
     <main>
-      <PaginationTable/>
+      <RowSelectionTable/>
     </main>
   );
 }
